56bcef89fa4bb5078f500c92f2d0ac71c7660f80
Ronny <ripunjoy.buddha@gmail.com>
2021-08-08 16:24:24 +0530

    modified header and added mainboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.scss'
 
-import Header from './components/header/Header'
+import Header from './page_sections/header/Header'
+import MainBoard from './page_sections/main-board/MainBoard'
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
+      <MainBoard/>
     </div>
   )
 }
---
+++ src/page_sections/main-board/MainBoard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './MainBoard.module.scss'
+
+const MainBoard = () => {
+    return (
+        <div className={styles.container}>
+            
+            
+        </div>
+    )
+}
+
+export default MainBoard
